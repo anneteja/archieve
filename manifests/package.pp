5958e6e4439d3093f26f1211b9a2d3ef8063b904
anneteja <anneteja49@gmail.com>
2018-09-05 20:59:02 +0000

    adding manifests

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,9 +1,8 @@
 class apache::package (
-        String $packagename = lookup({"name" => "apache.packagename", "default_value" => ""})
-        ) {
-        package { 'apache-package':
-                name   => $packagename,
-                ensure => present
-       }
+	String $packagename = lookup({"name" => "apache.packagename", "default_value" => $::apache::parameters::packagename})
+	) inherits ::apache::parameters {
+	package { 'apache-package':
+		ensure => present,
+		name   => $packagename,
+	}
 }
-
